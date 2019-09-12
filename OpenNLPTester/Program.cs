46d407a42083c186a7bb32031ed29dba4d916651
MrTheStarChild <mrthestarchild@gmail.com>
2019-09-11 20:14:15 -0500

    updated fixed ner generator, fixed but not working as expected.

--- OpenNLPTester/Program.cs
+++ OpenNLPTester/Program.cs
@@ -23,12 +23,24 @@ namespace OpenNLPTester
             Seperator("Tokenizer");
             // Test of Tokenizer with internal string.
             string tokenPath = $@"{modelsPath}EnglishTok.nbin";
-            string sentence = "- Sorry Mrs. Hudson, I'll skip the tea I'll be back in October 5th 2019.";
+            string sentence = "- Sorry Mrs. Hudson, I'll skip the tea I'll be back in October 5th 2019 malignant tumor.";
             EnglishMaximumEntropyTokenizer tokenizer = new EnglishMaximumEntropyTokenizer(tokenPath);
             string[] tokens = tokenizer.Tokenize(sentence);
             foreach (string token in tokens)
             {
                 Console.WriteLine(token);
+            }
+
+            Seperator("Sentence Splitter");
+            // Test of Sentence Splitter
+            // TODO: Fix is it not splitting the sentences
+            var paragraph = "Mr. & Mrs. Smith is a 2005 American romantic comedy action film. The film stars Brad Pitt and Angelina Jolie as a bored upper-middle class married couple. They are surprised to learn that they are both assassins hired by competing agencies to kill each other.";
+            string modelPath = $@"{modelsPath}EnglishTok.nbin";
+            var sentenceDetector = new EnglishMaximumEntropySentenceDetector(modelPath);
+            var sentences = sentenceDetector.SentenceDetect(paragraph);
+            foreach (string theSentence in sentences)
+            {
+                Console.WriteLine(theSentence);
             }
 
             Seperator("posTagger");
@@ -60,11 +72,11 @@ namespace OpenNLPTester
             string ner = nameFinder.GetNames(currentModels, sentence);
             Console.WriteLine(ner);
 
-            Seperator("Parser");
+            Seperator("Parse Tree");
             // TODO: Fix Parse and run secondary test. This is not working as expected.
             // test of the Parser
             EnglishTreebankParser parser = new EnglishTreebankParser(modelsPath);
-            Parse parse = parser.DoParse(sentence);
+            Parse parse = parser.DoParse(tokens);
             Console.WriteLine(parse);
 
             Seperator("Trainer");
@@ -72,25 +84,18 @@ namespace OpenNLPTester
             // The file with the training samples; works also with an array of files
             string trainingFile = @"C:\Users\nerab\source\repos\OpenNLPTester\OpenNLPTester\Resources\Training\NER\disease.train";
             // The number of iterations; no general rule for finding the best value, just try several!
-            int iterations = 5;
+            int iterations = 100;
             // The cut; no general rule for finding the best value, just try several!
-            int cut = 2;
-            // TODO: this is broken right now with a null pointer exception. can't figure out why?
+            int cut = 5;
+            // TODO: this is not working as expected
             // Train the model (can take some time depending on your training file size)
-            GisModel bestModel;
-            try
-            {
-                bestModel = MaximumEntropyNameFinder.TrainModel(trainingFile, iterations, cut);
-                // Persist the model to use it later
-                var outputFilePath = $@"{modelsPath}NameFind\";
-                new BinaryGisModelWriter().Persist(bestModel, outputFilePath);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"dumb: {e}");
-            }
-            
 
+            GisModel bestModel = MaximumEntropyNameFinder.TrainModel(trainingFile, iterations, cut);
+            // Persist the model to use it later
+            var outputFilePath = $@"{modelsPath}NameFind\disease.nbin";
+            new BinaryGisModelWriter().Persist(bestModel, outputFilePath);
+
+            Console.WriteLine("Done!");
             Console.ReadLine();
 
         }
